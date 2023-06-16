7a679168838d74141b67c73956854b6d47459799
hamzamiladin <hamzamiladin123@gmail.com>
2023-06-16 23:01:30 +0200

    Added basic mesh generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,45 +3,54 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    
-    public enum DrawMode{ NoiseMap, ColorMap};
+    public enum DrawMode
+    {
+        NoiseMap,
+        ColorMap,
+        Mesh
+    };
+
     public DrawMode drawMode;
-    public int width;
-    public int height;
+    
+    const int mapChunkSize = 241;
+    [Range(0,6)] public int LOD;
     public int seed;
     public float scale;
     public int octaves;
     public float persistence;
     public float lacunarity;
     public Vector2 offset;
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
     public TerrainType[] regions;
-    
+
     public bool autoUpdate = false;
-    
+
 
     public void Generate()
     {
-        float [,] noiseMap = Noise.GenerateNoiseMap(width, height, seed, scale, octaves, persistence, lacunarity, offset);
-        
+        float[,] noiseMap =
+            Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, scale, octaves, persistence, lacunarity, offset);
 
-        Color[] colorMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < mapChunkSize; x++)
             {
                 float currentHeight = noiseMap[x, y];
-                for(int i=0; i<regions.Length; i++)
+                for (int i = 0; i < regions.Length; i++)
                 {
-                    if(currentHeight <= regions[i].height)
+                    if (currentHeight <= regions[i].height)
                     {
-                        colorMap[y * width + x] = regions[i].color;
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
                         break;
                     }
                 }
             }
         }
 
-        
+
         DrawMap drawMap = FindObjectOfType<DrawMap>();
         if (drawMode == DrawMode.NoiseMap)
         {
@@ -49,25 +58,23 @@ public class MapGenerator : MonoBehaviour
         }
         else if (drawMode == DrawMode.ColorMap)
         {
-            drawMap.DrawTexture(TextureGenerator.TextureColorMap(colorMap, width, height));
+            drawMap.DrawTexture(TextureGenerator.TextureColorMap(colorMap, mapChunkSize, mapChunkSize));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            drawMap.DrawMesh(MeshGen.GenerateMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, LOD),
+                TextureGenerator.TextureColorMap(colorMap, mapChunkSize, mapChunkSize));
         }
     }
 
     private void OnValidate()
     {
-        if(width < 1)
-        {
-            width = 1;
-        }
-        if(height < 1)
-        {
-            height = 1;
-        }
-        if(lacunarity < 1)
+        if (lacunarity < 1)
         {
             lacunarity = 1;
         }
-        if(octaves < 0)
+
+        if (octaves < 0)
         {
             octaves = 0;
         }
---
+++ Assets/Scripts/MeshGen.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshGen 
+{
+    public static MeshData GenerateMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        float topLeftX = (width - 1) / -2f;
+        float topLeftZ = (height - 1) / 2f;
+        
+        int simplifyIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        int verticesPerLine = (width - 1) / simplifyIncrement + 1;
+        
+        MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
+        int vertexIndex = 0;
+        
+        for (int y = 0; y < height; y+=simplifyIncrement)
+        {
+            for(int x=0; x<width; x+=simplifyIncrement)
+            {
+                meshData.vertices[vertexIndex] = new Vector3(topLeftX +x, heightCurve .Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ-y);
+                meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+                
+                if(x < width - 1 && y < height - 1)
+                {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
+                }
+                
+                vertexIndex++;
+            }
+        }
+        return meshData;
+    }
+}
+
+public class MeshData
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+    public Vector2[] uvs;
+    
+    int index;
+    
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        uvs = new Vector2[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
+    
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[index] = a;
+        triangles[index + 1] = b;
+        triangles[index + 2] = c;
+        index += 3;
+    }
+    
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+}
